0e8a9814814ba5a2a5aef1aaccab12aab936f98b
asifally <asifally004@gmail.com>
2020-07-12 17:07:15 -0500

    Trying to fix something with the secret key

--- personal_portfolio/settings/base.py
+++ personal_portfolio/settings/base.py
@@ -18,7 +18,7 @@ SECRET_KEY = os.environ['SECRET_KEY']
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['asifally.com', 'http://127.0.0.1:8000/']
+ALLOWED_HOSTS = ['asifally.com', '127.0.0.1']
 
 
 # Application definition
--- personal_portfolio/settings/local.py
+++ personal_portfolio/settings/local.py
@@ -2,4 +2,4 @@ from .base import *
 
 DEBUG = True
 
-ALLOWED_HOSTS = ['asifally.com', 'http://127.0.0.1:8000/'] # or ['*']
+ALLOWED_HOSTS = ['asifally.com', '127.0.0.1'] # or ['*']
