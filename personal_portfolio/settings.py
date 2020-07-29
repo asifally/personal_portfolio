59e8c099c0f59bc27f38f54c8213ecd824b8773f
asifally <asifally004@gmail.com>
2020-07-29 16:31:05 -0500

    Finished second version of my site

--- personal_portfolio/settings.py
+++ personal_portfolio/settings.py
@@ -110,7 +110,11 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
-#STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]
-PROJECT_DIR = os.path.dirname(os.path.abspath(__file__))
-STATIC_ROOT =  os.path.join(PROJECT_DIR, 'static')
 STATIC_URL = '/static/'
+#Local dev
+STATICFILES_DIRS = [os.path.join(BASE_DIR, "/static/")]
+
+#Production dev
+#PROJECT_DIR = os.path.dirname(os.path.abspath(__file__))
+#STATIC_ROOT =  os.path.join(PROJECT_DIR, '/static/')
+
